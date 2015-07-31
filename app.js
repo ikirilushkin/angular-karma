0dc7dff1d69cdb4bca1c90fe966859669592f45e
Ilya Kirilushkin <ilya.kirilushkin@gmail.com>
2015-07-31 18:29:36 +0300

    create simple test

--- app.js
+++ app.js
@@ -2,5 +2,18 @@
     angular.module('app', [])
         .controller('MainController', [function(){
             console.log(2);
+        }])
+        .controller('PasswordController', ['$scope', function($scope){
+            $scope.password = '';
+            $scope.grade = function (){
+                var size = $scope.password.length;
+                if (size > 8) {
+                    $scope.strength = 'strong';
+                } else if (size > 3){
+                    $scope.strength = 'medium';
+                } else {
+                    $scope.strength = 'weak';
+                }
+            }
         }]);
 })();
